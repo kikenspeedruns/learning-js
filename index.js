9fa64105fc57c030333c5d7194b1942631e51ec4
kikenspeedruns <77496597+kikenspeedruns@users.noreply.github.com>
2022-11-22 15:04:52 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+
+// commenting
+/* multiline
+   comment */
+
+// printing
+// console.log("replthis dick in your mouth");
+
+// variables
+
+var myName = "Enzo";
+var myAge = 15;
+var myGender = "Male";
+
+console.log(myName, myAge, myGender);
+
+// how enby mfs write the same code block above: (its a reference to overriding variables)
+
+myName = "Sock"
+myAge = 16;
+myGender = undefined; // ill get to this soon
+
+console.log(myName, myAge, myGender);
+
+// math
+
+console.log(12 + 12);
+
+// variables has something to say
+
+var a = 12;
+var b = 13;
+
+console.log(a, "*", b, "=", a * b);
+
+// other types of math
+
+console.log("1 + 1 =", 1+1);
+console.log("1 - 1 =", 1-1);
+console.log("2 * 3 =", 2*3);
+console.log("3 / 7 =", 3/7);
+console.log("1 mod 2 =", 1%2);
+
+// types of variables
+
+var fruit = "strawberry"; // string
+var funny = 24; // number
+var house = {"Kitchen": 16^3, "Living Room": 24^3} // object
+
+// js is quite literal with types
+
+console.log(12 + "12");
+
+// also with numbers, too high makes it just read infinity
+
+console.log("512^512 =", Math.pow(512, 512))
